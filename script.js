e0054a545d90eedf1773c2b2ac912e20eccf2c06
michael305y <40887924+michael305y@users.noreply.github.com>
2025-05-01 17:39:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const  menuIcon = document.querySelector('#menu-icon');
-const  navigation = document.querySelector('.navigation');
+// const  menuIcon = document.querySelector('#menu-icon');
+// const  navigation = document.querySelector('.navigation');
 
 
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('bx-x');
-    navigation.classList.toggle('active');
-});
+// menuIcon.addEventListener('click', () => {
+//     menuIcon.classList.toggle('bx-x');
+//     navigation.classList.toggle('active');
+// });
